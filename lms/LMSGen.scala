4c98a314feecf5a79a617d656a75b018f7abf866
Thierry Coppey <tcknetwork@hotmail.com>
2013-09-19 10:50:15 +0200

    Nits.

--- lms/LMSGen.scala
+++ lms/LMSGen.scala
@@ -112,9 +112,9 @@ class LMSGen(cls:String="Query") extends ScalaGen(cls) {
     "\n}"
   }
 
-  def freshRef(/*impl: Impl,*/tp: Type): impl.Sym[_] = freshRefManifest(typeManifest(tp) /*, impl*/)
+  def freshRef(tp: Type): impl.Sym[_] = freshRefManifest(typeManifest(tp))
 
-  def freshRefManifest[T:Manifest](mf: Manifest[T] /*, impl: Impl*/): impl.Sym[T] = impl.fresh[T]
+  def freshRefManifest[T:Manifest](mf: Manifest[T]): impl.Sym[T] = impl.fresh[T]
 
   def typeManifest(tp:Type):Manifest[_] = tp match {
     case TypeLong => manifest[Long]
@@ -139,29 +139,4 @@ class LMSGen(cls:String="Query") extends ScalaGen(cls) {
   override def genSystem(s0:System):String = {
     maps=s0.maps; val r=super.genSystem(s0); maps=Nil; r
   }
-
-  //type Rep[+A] = A // to be replaced
-
-  //type Ctx = Map[String,Rep[_]]
-  //protected val ctx0 = Map[String,Rep[_]]()
-
-  /*
-  def mul[T](l:Rep[T],r:Rep[T]):Rep[T] = __mul(l,r)
-  def add[T](l:Rep[T],r:Rep[T]):Rep[T] = __add(l,r)
-  def cmp[T](l:Rep[T],r:Rep[T]):Rep[T]
-  def ex[T](e:Rep[T]):Rep[T]
-  def lift[T](n:String,e:Rep[T]):(Rep[T],Ctx)
-
-  def expr(ex:Expr,ctx:Ctx=ctx0):(Rep[_],Ctx) = ex match {
-    case Mul(l,r) => val (sl,cl)=expr(l,ctx); val (sr,cr)=expr(r,cl); (mul(sl,sr),cr)
-    case Add(l,r) => val (sl,cl)=expr(l,ctx); val (sr,cr)=expr(r,ctx); (add(sl,sr),cl++cr)
-    case Cmp(l,r,op) => val (sl,cl)=expr(l,ctx); val (sr,cr)=expr(r,ctx); (cmp(sl,sr),cl++cr)
-    case Exists(e) => val (se,ce)=expr(e,ctx); (ex(se),ce)
-    case Lift(n,e) => val (se,ce)=expr(e,ctx); val (sl,cl)=lift(n,se); (sl,cl++ce)
-    case AggSum(ks,e) => ks.toSet
-    case Apply(fn,tp,as) => as.flatMap(a=>a.collect(f)).toSet
-    case MapRef(n,tp,ks) => ks.toSet
-    case _ => (xx,ctx0)
-  }
-  */
 }
--- lms/M3Ops.scala
+++ lms/M3Ops.scala
@@ -21,6 +21,8 @@ trait M3Ops extends Base {
   def k3aggr[T:Manifest](map:K3M, f:(Rep[_],Rep[_])=>Rep[T]):Rep[T]
   def k3slice(map:K3M,part:Int,partKey:Rep[_]):K3M
   def k3clear(map:K3M):Rep[Unit]
+  // Function application
+  def k3apply(fn:String,args:List[Rep[_]],tp:Type):Rep[_]
 }
 
 trait M3OpsExp extends BaseExp with M3Ops {
@@ -30,20 +32,31 @@ trait M3OpsExp extends BaseExp with M3Ops {
   def k3get(map:K3M, key:List[Exp[_]],value_tp:Type) = K3Get(map,key,man(value_tp))
   def k3set(map:K3M, key:List[Exp[_]],value:Exp[_]) = K3Set(map,key,value)
   def k3add(map:K3M, key:List[Exp[_]],value:Exp[_]) = K3Add(map,key,value)
-  def k3foreach(map:K3M, f:(Exp[_],Exp[_])=>Exp[Unit]) = K3Foreach(map,f)
+  def k3foreach[K:Manifest,V:Manifest](map:K3M, f:(Exp[K],Exp[V])=>Exp[Unit]) = K3Foreach(map,f)
   def k3aggr[T:Manifest](map:K3M, f:(Exp[_],Exp[_])=>Exp[T]) = K3Aggr(map,f)
   def k3slice(map:K3M,part:Int,partKey:Exp[_]) = K3Slice(map,part,partKey)
   def k3clear(map:K3M) = K3Clear(map)
-
-  case class Named[T](n:String,mT:Manifest[T]) extends Def[T] {}
-  case class NewK3Temp[K,V](key:List[Type],value:Type,mK:Manifest[K],mV:Manifest[V]) extends Def[K3Temp[_,_]] {}
-  case class K3Get[T](map:K3M, key:List[Exp[_]],mt:Manifest[T]) extends Def[T] {}
-  case class K3Set(map:K3M, key:List[Exp[_]],value:Exp[_]) extends Def[Unit] {}
-  case class K3Add(map:K3M, key:List[Exp[_]],value:Exp[_]) extends Def[Unit] {}
-  case class K3Foreach(map:K3M, f:(Exp[_],Exp[_])=>Exp[Unit]) extends Def[Unit] {}
+  def k3apply(fn:String,args:List[Rep[_]],tp:Type) = fn match {
+    // inline here
+    /*
+    case "div" => //(x: Double): Double = if (x==0.0) 0.0 else 1.0 / x
+    case "listmax" // (v1: Long, v2: Long): Long = Math.max(v1, v2)
+                   // (v1: Double, v2: Double): Double = Math.max(v1, v2)
+    case "substring" //(s:String,b:Long,e:Long= -1) = if (e== -1) s.substring(b.toInt) else s.substring(b.toInt,e.toInt)
+    case "vec_length" //(x:Double, y:Double, z:Double):Double = Vector(x,y,z).length
+    */
+    case _ => K3Apply(fn,args,man(tp)) // fallback for large or unknown functions
+  }
+  case class Named[T](n:String,mT:Manifest[T]) extends Def[T]
+  case class NewK3Temp[K,V](key:List[Type],value:Type,mK:Manifest[K],mV:Manifest[V]) extends Def[K3Temp[_,_]]
+  case class K3Get[T](map:K3M, key:List[Exp[_]],mt:Manifest[T]) extends Def[T]
+  case class K3Set(map:K3M, key:List[Exp[_]],value:Exp[_]) extends Def[Unit]
+  case class K3Add(map:K3M, key:List[Exp[_]],value:Exp[_]) extends Def[Unit]
+  case class K3Foreach[K:Manifest,V:Manifest](map:K3M, f:(Exp[K],Exp[V])=>Exp[Unit]) extends Def[Unit] { val (k,v)=(fresh[K],fresh[V]); val body=f(k,v) }
   case class K3Aggr[T:Manifest](map:K3M, f:(Exp[_],Exp[_])=>Exp[T]) extends Def[T] {}
-  case class K3Slice(map:K3M,part:Int,partKey:Exp[_]) extends Def[K3Map[_,_]] {}
-  case class K3Clear(map:K3M) extends Def[Unit] {}
+  case class K3Slice(map:K3M,part:Int,partKey:Exp[_]) extends Def[K3Map[_,_]]
+  case class K3Clear(map:K3M) extends Def[Unit]
+  case class K3Apply[T](name:String,args:List[Rep[_]],mT:Manifest[T]) extends Def[T]
 }
 
 trait ScalaGenM3Ops extends ScalaGenBase {
@@ -57,10 +70,11 @@ trait ScalaGenM3Ops extends ScalaGenBase {
     case K3Get(m,ks,_) => emitValDef(sym, quote(m)+".get("+tup(ks map quote)+")")
     case K3Set(m,ks,v) => stream.println(quote(m)+".set("+tup(ks map quote)+","+quote(v)+")")
     case K3Add(m,ks,v) => stream.println(quote(m)+".add("+tup(ks map quote)+","+quote(v)+")")
-    //case K3Foreach(m,f) => stream.println(quote(m)+".foreach("+quote(f)+")")
+    case f@K3Foreach(m,_) => stream.println(quote(m)+".foreach( ("+quote(f.k)+","+quote(f.v)+") => "+quote(f.body)+")")
     //case K3Aggr(m,f) => emitValDef(sym, quote(m)+".aggr("+quote(f)+")")
     case K3Slice(m,p,pk) => emitValDef(sym, quote(m)+".slice("+p+","+quote(pk)+")")
     case K3Clear(m) => stream.println(quote(m)+".clear")
+    case K3Apply(fn,args,_) => emitValDef(sym,fn+"("+(args map quote).mkString(",")+")")
     case _ => super.emitNode(sym,rhs)
   }
 }
