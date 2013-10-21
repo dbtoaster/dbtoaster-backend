0e9c4827c18e64a9f2521381f1abcde0cc6e618a
Mohammad Dashti <mdashti@gmail.com>
2013-10-21 15:48:20 +0200

    FIXED - Using new inlining level directives.

--- src/codegen/K3MapCommons.scala
+++ src/codegen/K3MapCommons.scala
@@ -16,8 +16,7 @@ object K3MapCommons {
 
   def isInliningInSpecializedLevel = (InliningLevel == InliningLevelSpecialized)
 
-  var InliningLevel = InliningLevelSpecialized
-  //def InliningLevel = ddbt.Compiler.inl
+  def InliningLevel = ddbt.Compiler.inl
 
   /**
    * The default initial capacity - MUST be a power of two.
