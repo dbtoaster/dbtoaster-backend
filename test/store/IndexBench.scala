e1949d30d53993417b767c9a5ec7b8d8b417e09f
Thierry Coppey <tcknetwork@hotmail.com>
2014-03-07 21:16:58 +0100

    Nits.

--- test/store/IndexBench.scala
+++ test/store/IndexBench.scala
@@ -17,60 +17,67 @@ object IndexBench {
       s.index(1,new IdxHashL(s,1,false,lf_nu.toFloat))
       s.index(2,new IdxHashL(s,2,false,lf_nu.toFloat))
     } else {
-      s.index(0,new IdxHash(s,0,true,lf_u.toFloat))
+      s.index(0,new IdxHashL(s,0,true,lf_u.toFloat))
       s.index(1,new IdxHash(s,1,false,lf_nu.toFloat))
       s.index(2,new IdxHash(s,2,false,lf_nu.toFloat))
     }
     s
   }
-  // Keep elements in an array (reduces GC?)
-  def array(N:Int,M:Int) = {
-    val a = new Array[E](N)
-    val L = N/M
-    var i=0; while (i<N) { a(i)=E(i%L,i,i); i+=1; }
-    a
-  }
   // Assumes 80% read / 20% write workload
-  def work(s:Store[E], a:Array[E], M:Int) = { s.clear; System.gc(); ns(()=>{
-    val N = a.size; s.clear
-    var i=0
-    var e:E=null.asInstanceOf[E]
-    i=0; while(i<N) { s.insert(a(i)); i+=1; } // 10% insert
-    i=0; while(i<N) { e=s.get(0,s.get(1,s.get(2,a(i)))); i+=1 } // 30% get
-    i=0; val L=N/M;
-    while(i<L) {
-      s.slice(0,a(i),x => e=x) // ~50% slicing (well a bit less with slice 0)
-      s.slice(2,a(i),x => e=x)
-      s.slice(2,a(i),x => e=x)
-      s.slice(1,a(i),x => e=x)
-      s.slice(1,a(i),x => e=x)
-      s.slice(1,a(i),x => e=x)
-      s.slice(1,a(i),x => e=x)
-      i+=1
-    }
-    i=0; while(i<N) { s.delete(a(i)); i+=1; } // 10% delete
-  })._1 }
+  def work(s:Store[E], a:Array[E], M:Int) = {
+    s.clear; System.gc();
+    ns(()=>{
+      val N = a.size; s.clear
+      var i=0
+      var e:E=null.asInstanceOf[E]
+      i=0; while(i<N) { s.insert(a(i)); i+=1; } // 10% insert
+      i=0; while(i<N) { e=s.get(0,s.get(1,s.get(2,a(i)))); i+=1 } // 30% get
+      i=0; val L=N/M;
+      (0 until 10).foreach { x => while(i<L) {
+        //s.slice(0,a(i),x => e=x) // ~50% slicing (well a bit less with slice 0)
+        //s.slice(2,a(i),x => e=x)
+        //s.slice(2,a(i),x => e=x)
+        s.slice(1,a(i),x => e=x)
+        s.slice(1,a(i),x => e=x)
+        s.slice(1,a(i),x => e=x)
+        s.slice(1,a(i),x => e=x)
+        s.slice(1,a(i),x => e=x)
+        i+=1
+      }}
+      i=0; while(i<N) { s.delete(a(i)); i+=1; } // 10% delete
+    })._1
+  }
 
-  def test(S:Int,N:Int,M:Int,list:Boolean,lf_u:Double,lf_nu:Double,out:PrintStream=null) {
-    val s = store(list,lf_u,lf_nu)
-    val a = array(N,M)
-    val ts = (0 until S).map { x => work(s,a,M) }.sorted
-    val med=if (S%2==1) ts(S/2) else (ts(S/2)+ts(S/2-1))/2
-    val str = List(S,N,M,if (list) 1 else 0,lf_u,lf_nu,time(ts(0)),time(med),time(ts(S-1))).mkString(",")
+  def test(S:Int,N:Int,M:Int,lf_u:Double,lf_nu:Double,out:PrintStream=null) {
+    val L=N/M // Keep elements in an array (to refer after)
+    val a=new Array[E](N); var i=0; while (i<N) { a(i)=E(i%L,i,i); i+=1; }
+
+    val s1 = store(false,lf_u,lf_nu)
+    val s2 = store(true,lf_u,lf_nu)
+    val ts1 = (0 until S).map { x => work(s1,a,M) }.sorted
+    val ts2 = (0 until S).map { x => work(s2,a,M) }.sorted
+    val (med1,med2)=if (S%2==1) (ts1(S/2),ts2(S/2)) else (  (ts1(S/2)+ts1(S/2-1))/2,  (ts2(S/2)+ts2(S/2-1))/2  )
+    val str = List(S,N,M,lf_u,lf_nu,
+                   time(ts1(0)),time(med1),time(ts1(S-1)), // IdxHash
+                   time(ts2(0)),time(med2),time(ts2(S-1))  // IdxHashL
+              ).mkString(",")
     println(str); if (out!=null) out.println(str);
   }
 
   def main(args:Array[String]) {
     val S = 10
-    val N = 100000
+    val N = 1000000
     val out = new PrintStream(new File("test/store/out.txt"))
 
-    var f=.3; while (f<3) { test(S,N,10,false,f,4,out); f+=.05 }
-    out.close ();
+    // lf_u is best in 0.7-1.5
+    // lf_nu
+
+    test(S,N,50,.75,4,null); // warm-up
+
+    var v=100; while (v<1000) { test(S,N,v,.75,4,out); v+=100 }
+    out.close;
 
     //val s2 = store(true)
-
-
 
 
     //class Store[E<:Entry](val idxs:Array[Idx[E]], val ops:Array[EntryIdx[E]]=null)(implicit cE:ClassTag[E]) {
--- test/store/graph.m
+++ test/store/graph.m
@@ -5,18 +5,16 @@ data=load('out.txt');
 S=data(:,1);
 N=data(:,2);
 M=data(:,3);
-L=data(:,4);
-lu=data(:,5);
-lnu=data(:,6);
-ta=data(:,7);
-tl=data(:,8);
-tu=data(:,9);
+Lu=data(:,4);
+Lnu=data(:,5);
 
-x=lu;
+x=M;
 
 figure;
 hold on;
-plot(x,ta);
-plot(x,tl);
-plot(x,tu)
-% plot(x,la,'bo',[1:500]*2,m,'-r','LineWidth',2)
+plot(x,data(:, 6),'r-.'); % IdxHash
+plot(x,data(:, 7),'r');
+plot(x,data(:, 8),'r-.')
+plot(x,data(:, 9),'b-.'); % IdxHashL
+plot(x,data(:,10),'b');
+plot(x,data(:,11),'b-.');
