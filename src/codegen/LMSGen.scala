c95ebeb1ef5abecb7364cfaf9fedf835f05bec9b
Mohammad Dashti <mdashti@gmail.com>
2014-04-23 10:08:48 +0200

    Fixed compilation problem when LMS is turned off

--- src/codegen/LMSGen.scala
+++ src/codegen/LMSGen.scala
@@ -2,8 +2,21 @@ package ddbt.codegen
 import ddbt.ast._
 
 /** Stub for conditional compilation, actual implementation in lms/ folder */
-case class LMSGen(cls:String="Query") extends ScalaGen(cls) {
-  override def apply(s:M3.System) = sys.error(
+abstract class LMSGen(override val cls:String="Query") extends CodeGen {
+  override def apply(s0:M3.System):String = sys.error(
+    "Set 'ddbt.lms=1' in conf/ddbt.properties to enable LMS code generation"
+  )
+}
+
+
+class LMSScalaGen(cls:String="Query") extends LMSGen(cls) with IScalaGen{
+  override def apply(s0:M3.System):String = sys.error(
+    "Set 'ddbt.lms=1' in conf/ddbt.properties to enable LMS code generation"
+  )
+}
+
+class LMSCppGen(cls:String="Query") extends LMSGen(cls) with ICppGen{
+  override def apply(s0:M3.System):String = sys.error(
     "Set 'ddbt.lms=1' in conf/ddbt.properties to enable LMS code generation"
   )
 }
