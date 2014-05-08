05f5408ed68d1738232a7b54af31c13c6c6b182b
Andres Notzli <andres.noetzli@gmail.com>
2014-05-08 14:49:51 +0300

    Switch to change g++ compiler, needed for OSX 10.9 (Mavericks)

--- src/Utils.scala
+++ src/Utils.scala
@@ -57,7 +57,7 @@ object Utils {
 
   // C++ compiler wrapper
   def cppCompiler(out:String,cPath:String,boost:String,cppLibDir:String) = {
-    val as = List("g++",cppLibDir+"/main.cpp","-include",out,"-o",cPath,"-O3","-lpthread","-ldbtoaster","-I"+cppLibDir,"-L"+cppLibDir) :::
+    val as = List(prop("gpp","g++"),cppLibDir+"/main.cpp","-include",out,"-o",cPath,"-O3","-lpthread","-ldbtoaster","-I"+cppLibDir,"-L"+cppLibDir) :::
              List("program_options","serialization","system","filesystem","chrono",Utils.prop("lib_boost_thread","thread")).map("-lboost_"+_) ::: // thread-mt
              (if (boost==null) Nil else List("-I"+boost+"/include","-L"+boost+"/lib"))
     //make DBT c++ library
