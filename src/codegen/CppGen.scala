1eb3a71192224fe00429d5d6c862c671af4830b3
Mohammad Dashti <mdashti@gmail.com>
2014-05-13 16:24:03 +0200

    Closed #64

--- src/codegen/CppGen.scala
+++ src/codegen/CppGen.scala
@@ -554,6 +554,12 @@ trait ICppGen extends IScalaGen {
     "\n"+
     "  protected:\n"+
     "    data_t data;\n"+
+    "};\n"+
+    "class "+cls+" : public Program\n"+
+    "{\n"+
+    "  public:\n"+
+    "    "+cls+"(int argc = 0, char* argv[] = 0) : Program(argc,argv) {\n"+
+    "    }\n"+
     "};\n"
   }
 
