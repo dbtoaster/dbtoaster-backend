1e3fbb26415ba3c0e1b300f565dff6efa42138f8
Mohammad Dashti <mdashti@gmail.com>
2014-06-13 01:14:50 +0200

    Added -Wno-unused-variable to cppCompiler in Utils. g++ will produce warnings while running tpch20 and tpch21 complaining about unused variables. We don’t want to implement dead-code elimination now, as it would be done later using LMS.

--- src/Utils.scala
+++ src/Utils.scala
@@ -54,7 +54,7 @@ object Utils {
 
   // C++ compiler wrapper
   def cppCompiler(out:String,cPath:String,boost:String,cppLibDir:String) = {
-    val as = (List(prop("gpp","g++"),cppLibDir+"/main.cpp","-Wall","-std=c++11","-include",out,"-o",cPath,"-O3","-lpthread","-ldbtoaster","-I"+cppLibDir,"-L"+cppLibDir) :::
+    val as = (List(prop("gpp","g++"),cppLibDir+"/main.cpp","-Wall","-Wno-unused-variable","-std=c++11","-include",out,"-o",cPath,"-O3","-lpthread","-ldbtoaster","-I"+cppLibDir,"-L"+cppLibDir) :::
              List("program_options","serialization","system","filesystem","chrono","thread").map("-lboost_"+_+Utils.prop("lib_boost_thread","")) ::: // thread-mt
              (if (boost==null) Nil else List("-I"+boost+"/include","-L"+boost+"/lib")) ::: cppOpts).filter(_!="")
     //make DBT c++ library
