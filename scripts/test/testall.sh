b01cb8ace36a67e74b62ee83bba256c66bbd885a
Mohammad Dashti <mdashti@gmail.com>
2014-07-28 20:34:12 +0200

    Added all other queries which finish in reasonable amount of time to testall.sh

--- scripts/test/testall.sh
+++ scripts/test/testall.sh
@@ -26,11 +26,21 @@ echo "Executing test_cppnew_big_O3..."
 ./query_test.rb -t cppnew --dataset big --O3 #> test_cppnew_big_O3.txt
 echo "Executing test_cppnew_big_del_O3..."
 ./query_test.rb -t cppnew --dataset big_del --O3 #> test_cppnew_big_del_O3.txt
+./query_test.rb -t cppnew --dataset big_del --O3 tpch5
+./query_test.rb -t cppnew --dataset big_del --O3 tpch17a
+./query_test.rb -t cppnew --dataset big_del --O3 tpch18
+./query_test.rb -t cppnew --dataset big_del --O3 tpch18a
+./query_test.rb -t cppnew --dataset big_del --O3 tpch21
 echo "Executing test_cppnew_big_O2..."
 ./query_test.rb -t cppnew --dataset big --O2 #> test_cppnew_big_O2.txt
+./query_test.rb -t cppnew --dataset big --O2 tpch11
 echo "Executing test_cppnew_big_del_O2..."
 ./query_test.rb -t cppnew --dataset big_del --O2 #> test_cppnew_big_del_O2.txt
+./query_test.rb -t cppnew --dataset big_del --O2 ssb4
 echo "Executing test_cppnew_big_O1..."
 ./query_test.rb -t cppnew --dataset big --O1 #> test_cppnew_big_O1.txt
+./query_test.rb -t cppnew --dataset big --O2 tpch11
 echo "Executing test_cppnew_big_del_O1..."
 ./query_test.rb -t cppnew --dataset big_del --O1 #> test_cppnew_big_del_O1.txt
+./query_test.rb -t cppnew --dataset big_del --O1 tpch18a
+./query_test.rb -t cppnew --dataset big_del --O1 ssb4
