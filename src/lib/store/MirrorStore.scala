9107af7b5e0f94d67c19f78340452f47815340fc
Mohammad Dashti <mdashti@gmail.com>
2014-12-02 15:56:33 +0100

    MirrorStore for lifting Store

---
+++ src/lib/store/MirrorStore.scala
@@ -0,0 +1,24 @@
+package ddbt.lib.store
+import scala.reflect._
+
+import ddbt.Utils.ind
+import scala.collection.mutable.HashMap
+import ch.epfl.data.pardis.annotations._
+
+@needs[Array[_]]
+@deep
+// @reflect[Store[_]]
+class MStore[E<:Entry](val idxs:Array[E], val ops:Array[E]) {
+  private val n = idxs.length
+  // def this(n:Int) = this(new Array[Idx[E]](n),null)(cE)
+  // def this(n:Int,ops:Array[EntryIdx[E]]) = this(new Array[Idx[E]](n),ops)(cE)
+  def unsafeInsert(idx:Int,e:E):Unit = ???
+  def insert(e:E):Unit = ???
+  def update(e:E):Unit = ???
+  def delete(e:E):Unit = ???
+  def get(idx:Int,key:E):E = ???
+  def foreach(f:E=>Unit):Unit = ???
+  def slice(idx:Int,key:E,f:E=>Unit): Unit = ???
+  def range(idx:Int,min:E,max:E,withMin:Boolean,withMax:Boolean,f:E=>Unit): Unit = ???
+  def delete(idx:Int,key:E):Unit = ???
+}
