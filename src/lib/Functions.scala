4957c20db56c70ea4a90163bfb1faa47a005b8c5
Milos Nikolic <milos.nikolic@epfl.ch>
2015-02-06 11:31:35 +0100

    Date type uses Long instead of java.util.Date.

--- src/lib/Functions.scala
+++ src/lib/Functions.scala
@@ -1,5 +1,4 @@
 package ddbt.lib
-import java.util.Date;
 
 /**
  * These are user-library functions, prefixed with 'U' (to avoid name clashes).
@@ -15,89 +14,88 @@ import java.util.Date;
 object Functions {
   import scala.language.implicitConversions
   // Implicit conversions
-  implicit def boolConv(b:Boolean):Long = if (b) 1L else 0L
-  implicit def dateOps(d:Date):DateWrapper = new DateWrapper(d)
-  class DateWrapper(d:Date) {
-    def < (d2:Date) = d.getTime < d2.getTime
-    def > (d2:Date) = d.getTime > d2.getTime
-    def <= (d2:Date) = d.getTime <= d2.getTime
-    def >= (d2:Date) = d.getTime >= d2.getTime
+  implicit def boolConv(b: Boolean): Long = if (b) 1L else 0L
+
+  def Udate(str: String): Long = {
+    val s = str.split("-")
+    (s(0).toLong * 100 + s(1).toLong) * 100 + s(2).toLong
   }
 
-  // User functions that can be called using apply
-  // These are prefixed by U (user) to avoid name clash with possible other functions
-  def Udate(str:String):Date = {
-    val s=str.split("-")
-    new java.util.GregorianCalendar(s(0).toInt,s(1).toInt - 1,s(2).toInt).getTime();
-  }
+  private val regExpCache = new java.util.HashMap[String, java.util.regex.Pattern]()
 
-  private val re_cache = new java.util.HashMap[String, java.util.regex.Pattern]()
-  def Uregexp_match(re:String, str:String): Long = if ((re_cache.get(re) match {
-    case null => val p=java.util.regex.Pattern.compile(re); re_cache.put(re,p); p
+  def Uregexp_match(re: String, str: String): Long = if ((regExpCache.get(re) match {
+    case null => 
+      val p = java.util.regex.Pattern.compile(re)
+      regExpCache.put(re, p)
+      p
     case p => p
   }).matcher(str).find) 1L else 0L
-  def Upreg_match(p:java.util.regex.Pattern, str:String): Long = if (p.matcher(str).find) 1L else 0L
 
-  def Udiv(x: Double): Double = if (x==0.0) 0.0 else 1.0 / x
-  def UmulDbl(x: Double, y:Double): Double = x * y
-  def UmulLng(x: Long, y:Long): Long = x * y
+  def Upreg_match(p: java.util.regex.Pattern, str: String): Long = 
+    if (p.matcher(str).find) 1L else 0L
+
+  def Udiv(x: Long): Double = if (x == 0L) 0.0 else 1.0 / x
+  def Udiv(x: Double): Double = if (x == 0.0) 0.0 else 1.0 / x
+
+  def Umul(x: Double, y: Double): Double = x * y
+  def Umul(x: Long, y: Long): Long = x * y
+
   def Ulistmax(v1: Long, v2: Long): Long = Math.max(v1, v2)
   def Ulistmax(v1: Double, v2: Double): Double = Math.max(v1, v2)
 
-  def Udate_part(field:String, date:java.util.Date): Long = {
-    val c = java.util.Calendar.getInstance; c.setTime(date)
-    field.toLowerCase match {
-      case "year"  => c.get(java.util.Calendar.YEAR)
-      case "month" => c.get(java.util.Calendar.MONTH)
-      case "day"   => c.get(java.util.Calendar.DAY_OF_MONTH)
-      case p => throw new Exception("Invalid date part: "+p)
-    }
+  def Udate_part(field: String, date: Long): Long = field.toLowerCase match {
+    case "year"  => (date / 10000)
+    case "month" => (date / 100) % 100
+    case "day"   => (date % 100)
+    case p       => throw new Exception("Invalid date part: " + p)
   }
 
-  def Usubstring(s:String,b:Long,e:Long= -1) = if (e== -1) s.substring(b.toInt) else s.substring(b.toInt,e.toInt)
+  def Usubstring(s: String, b: Long, e: Long = -1L) = 
+    if (e == -1L) s.substring(b.toInt) else s.substring(b.toInt, e.toInt)
 
-  def Uvec_length(x:Double, y:Double, z:Double):Double = Vector(x,y,z).length
-  // def Uvec_dot(x1:Double, y1:Double, z1:Double, x2:Double, y2:Double, z2:Double) = Vector(x1,y1,z1) * Vector(x2,y2,z2)
+  def Uvec_length(x: Double, y: Double, z: Double): Double = UVector(x,y,z).length  
+  // def Uvec_lengthDouble(x:Double, y:Double, z:Double): Double = UVector(x,y,z).length
+  // def Uvec_dot(x1:Double, y1:Double, z1:Double, x2:Double, y2:Double, z2:Double) = UVector(x1,y1,z1) * UVector(x2,y2,z2)
 
-  private val PI = 3.141592653589793238462643383279502884
-  def Uradians(degree:Double) = degree * PI / 180
-  def Udegrees(radian:Double) = radian * 180 / PI
-  def Upow(x:Double, y:Double) = math.pow(x, y)
-  // def Usqrt(x:Double):Double = math.sqrt(x)
-  def Ucos(x:Double):Double = math.cos(x)
-  def Uvector_angle(x1:Double, y1:Double, z1:Double, x2:Double, y2:Double, z2:Double):Double = {
-    val v1 = Vector(x1, y1, z1)
-    val v2 = Vector(x2, y2, z2)
+  private val PI = Math.PI
+  def Uradians(degree: Double) = degree * PI / 180
+  def Udegrees(radian: Double) = radian * 180 / PI
+  def Upow(x: Double, y: Double) = Math.pow(x, y)
+  // def Usqrt(x: Double): Double = Math.sqrt(x)
+  def Ucos(x: Double): Double = Math.cos(x)
+  def Uvector_angle(x1: Double, y1: Double, z1: Double, x2: Double, y2: Double, z2: Double): Double = {
+    val v1 = UVector(x1, y1, z1)
+    val v2 = UVector(x2, y2, z2)
     v1.angle(v2)
   }
 
-  def Udihedral_angle(x1:Double, y1:Double, z1:Double,
-                      x2:Double, y2:Double, z2:Double,
-                      x3:Double, y3:Double, z3:Double,
-                      x4:Double, y4:Double, z4:Double) = {
-    val p1 = Vector(x1, y1, z1)
-    val p2 = Vector(x2, y2, z2)
-    val p3 = Vector(x3, y3, z3)
-    val p4 = Vector(x4, y4, z4)
+  def Udihedral_angle(x1: Double, y1: Double, z1: Double,
+                      x2: Double, y2: Double, z2: Double,
+                      x3: Double, y3: Double, z3: Double,
+                      x4: Double, y4: Double, z4: Double) = {
+    val p1 = UVector(x1, y1, z1)
+    val p2 = UVector(x2, y2, z2)
+    val p3 = UVector(x3, y3, z3)
+    val p4 = UVector(x4, y4, z4)
     val v1 = p2 - p1
     val v2 = p3 - p2
     val v3 = p4 - p3
     val n1 = v1 * v2
     val n2 = v2 * v3
-    math.atan2(v2.length * v1(n2), n1(n2))
+    Math.atan2(v2.length * v1(n2), n1(n2))
   }
 
-  case class Vector(x:Double, y:Double, z:Double) {
-    def length:Double = math.sqrt(x*x + y*y + z*z)
-    def apply(v:Vector) = x*v.x + y*v.y + z*v.z // dot
-    def -(v:Vector) = Vector(x-v.x, y-v.y, z-v.z)
-    def *(v:Vector) = Vector(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x)
-    def angle(v:Vector) = math.acos ( apply(v) / (length * v.length) )
+  case class UVector(x: Double, y: Double, z: Double) {
+    def length: Double = Math.sqrt(x * x + y * y + z * z)
+    def apply(v: UVector) = x * v.x + y * v.y + z * v.z // dot
+    def -(v: UVector) = UVector(x - v.x, y - v.y, z - v.z)
+    def *(v: UVector) = UVector(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x)
+    def angle(v: UVector) = Math.acos( apply(v) / (length * v.length) )
   }
 
-  def Uhash(n:Long):Long = { // mddb/query2_full.sql
-    var v:Long = n * 3935559000370003845L + 2691343689449507681L
-    v ^= v >> 21; v^= v << 37; v ^= v >> 4
+  def Uhash(n: Long): Long = { // mddb/query2_full.sql
+    var v: Long = n * 3935559000370003845L + 2691343689449507681L
+    v ^= v >> 21; v ^= v << 37; v ^= v >> 4
     v *= 4768777413237032717L
     v ^= v >> 20; v^= v << 41; v ^= v >> 5
     v
--- src/lib/ManifestHelper.scala
+++ src/lib/ManifestHelper.scala
@@ -5,48 +5,49 @@ import ddbt.lib.store._
 
 /* Helper to convert AST types into manifests */
 object ManifestHelper {
-  def man(tp:Type):Manifest[_] = tp match {
-    case TypeLong => manifest[Long]
+
+  def man(tp: Type): Manifest[_] = tp match {
+    case TypeLong   => manifest[Long]
     case TypeDouble => manifest[Double]
     case TypeString => manifest[String]
-    case TypeDate => manifest[java.util.Date]
-    case _ => sys.error("No manifest for "+tp)
+    case TypeDate   => manifest[Long]    //java.util.Date
+    case _ => sys.error("No manifest for " + tp)
   }
-  def man(ts:List[Type]):Manifest[_] = ts.size match {
+
+  def man(ts: List[Type]): Manifest[_] = ts.size match {
     case 1 => man(ts.head)
-    case _ => val ms:List[Manifest[_]] = ts map man
-      val cls:java.lang.Class[_] = Class.forName("scala.Tuple"+ts.size)
-      scala.reflect.ManifestFactory.classType(cls,ms.head,ms.tail:_*)
+    case _ => 
+      val ms: List[Manifest[_]] = ts map man
+      val cls: java.lang.Class[_] = Class.forName("scala.Tuple" + ts.size)
+      scala.reflect.ManifestFactory.classType(cls, ms.head, ms.tail : _*)
   }
-  def manEntry(ts:List[Type]):Manifest[Entry] = {
-    val ms:List[Manifest[_]] = ts map man
-    val cls:java.lang.Class[_] = Class.forName("ddbt.lib.store.SEntry"+ts.size)
-    scala.reflect.ManifestFactory.classType(cls,ms.head,ms.tail:_*).asInstanceOf[Manifest[Entry]]
-  }
-  def manStore(ts:List[Type]):Manifest[Store[Entry]] = manStore(manEntry(ts))
 
-  def manStore(entryMan:Manifest[Entry]):Manifest[Store[Entry]] = {
-    val cls:java.lang.Class[_] = Class.forName("ddbt.lib.store.Store")
-    scala.reflect.ManifestFactory.classType(cls,entryMan).asInstanceOf[Manifest[Store[Entry]]]
+  def manEntry(ts: List[Type]): Manifest[Entry] = {
+    val ms: List[Manifest[_]] = ts map man
+    val cls: java.lang.Class[_] = Class.forName("ddbt.lib.store.SEntry" + ts.size)
+    scala.reflect.ManifestFactory.classType(cls, ms.head, ms.tail : _*).asInstanceOf[Manifest[Entry]]
   }
-  def manContainer(ts:List[Type]):Manifest[Store[Entry]] = manStore(manEntry(ts))
 
-  def manContainer(entryMan:Manifest[Entry]):Manifest[Store[Entry]] = {
-    val cls:java.lang.Class[_] = Class.forName("ddbt.lib.store.Container")
-    scala.reflect.ManifestFactory.classType(cls,entryMan).asInstanceOf[Manifest[Store[Entry]]]
+  def manStore(ts: List[Type]): Manifest[Store[Entry]] = manStore(manEntry(ts))
+
+  def manStore(entryMan: Manifest[Entry]): Manifest[Store[Entry]] = {
+    val cls: java.lang.Class[_] = Class.forName("ddbt.lib.store.Store")
+    scala.reflect.ManifestFactory.classType(cls, entryMan).asInstanceOf[Manifest[Store[Entry]]]
   }
+
   def zero(tp: Type) = tp match {
-    case TypeLong => 0L
+    case TypeLong   => 0L
     case TypeDouble => 0.0
     case TypeString => ""
-    case TypeDate => new java.util.Date()
+    case TypeDate   => 0L         // new java.util.Date()
     case _ => sys.error("Bad Type")
   }
+
   def zero(m: Manifest[_]) = m.toString match {
-    case "Long" => 0L
+    case "Long"   => 0L
     case "Double" => 0.0
     case "String" => ""
-    case "java.util.Date" => new java.util.Date()
+    case "java.util.Date" => 0L   // new java.util.Date()
     case _ => sys.error("Bad manifest")
   }
 }
