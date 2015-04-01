55de1be6cc69ac205b2ab2f9fba1289069008287
Khue Vu <khuevu@gmail.com>
2015-04-01 11:43:43 +0200

    Add store watched code generation when insert and update store entries

--- lms/M3StoreOps.scala
+++ lms/M3StoreOps.scala
@@ -44,6 +44,7 @@ trait M3StoreOpsExp extends BaseExp
   val USE_STORE1 = true // whether we specialize temporary maps in Store1
   val USE_UNIQUE_INDEX_WHEN_POSSIBLE = true
   val STORE_TYPE = "M3StoreOpsExp.STORE_TYPE"
+  val STORE_WATCHED = "StoreOps.watched"
 
   def named(name: String, tp: Type, mutable: Boolean = false) = 
     named(name, mutable)(man(tp))
@@ -70,7 +71,7 @@ trait M3StoreOpsExp extends BaseExp
     s.attributes.put(STORE_TYPE, storeType)
   }
 
-  def m3add[E <: Entry](map: Rep[Store[E]], ent: Rep[E])(implicit m: Manifest[E]) = {    
+  def m3add[E <: Entry](map: Rep[Store[E]], ent: Rep[E])(implicit m: Manifest[E]) = {
     val n = m.typeArguments.size
     val lastMan = m.typeArguments.last
     val entVal = ent.get(n)
@@ -126,7 +127,15 @@ trait M3StoreOpsExp extends BaseExp
               __ifThenElse(
                 __equal(currentEnt, unit(null)),
                 stUnsafeInsert(map, ent, idx), 
-                { currentEnt += (n, entVal)
+                {
+                  val watched = map.asInstanceOf[Sym[_]].attributes.get(STORE_WATCHED).asInstanceOf[Option[Boolean]].getOrElse(false)
+                  if (watched) {
+                    val oldEnt = steCopy(currentEnt)
+                    currentEnt += (n, entVal)
+                    steLogUpdate(map, oldEnt, currentEnt)
+                  } else {
+                    currentEnt += (n, entVal)
+                  }
                   val currentEntVal = currentEnt.get(n)
                   __ifThenElse(
                     __equal(currentEntVal, unit(zero(lastMan))),
@@ -287,7 +296,8 @@ trait ScalaGenM3StoreOps extends ScalaGenBase
 
   override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
     case Named(n) => /*emitValDef(sym, n);*/ sym.attributes.update(nameAttr, n)
-    case StUnsafeInsert(x, e, i) => 
+    case StUnsafeInsert(x, e, i) =>
+      println("Sym " + quote(sym, true) + " x " + quote(x, true) + " unsafeInsert")
       emitValDef(sym, quote(x) + ".unsafeInsert(" + i + "," + quote(e) + ")")
     case M3Add(s, e) =>
       stream.println(quote(s) + ".add(" + quote(e) + ")")
