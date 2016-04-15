e9644fc36c03dfd2cca66b86949dfce47d924624
Sachin Basil John <sachinbjohn@gmail.com>
2016-04-15 17:31:01 +0200

    Added DeepMirrorEntryIdx

--- ddbtoaster/src/lib/store/MirrorEntryIdx.scala
+++ ddbtoaster/src/lib/store/MirrorEntryIdx.scala
@@ -6,15 +6,15 @@ import ch.epfl.data.sc.pardis.annotations.{reflect, deep}
   * Created by sachin on 15.04.16.
   */
 @deep
-abstract class MirrorEntryIdx[E <: Entry] {
+abstract class MirrorEntryIdx[E] {
   def cmp(e1: E, e2: E): Int
   def hash(e: E): Int
 }
-//
-//object MirrorEntryIdx {
-//  def apply[E](h: (E => Int), c: ((E, E) => Int)): MirrorEntryIdx[E] = ???
-//}
-//
+
+object MirrorEntryIdx {
+  def apply[E](h: (E => Int), c: ((E, E) => Int)): MirrorEntryIdx[E] = ???
+}
+
 //@deep
 //case class MirrorGenericOps(val cols: List[Int] = List()) extends MirrorEntryIdx[MirrorGenericEntry] {
 //  def hash(e: MirrorGenericEntry): Int = ???
