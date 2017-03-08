d6d13dde77ba5edc5401c4a4b94b5f5ecc2e9f12
amirsh <amir.shaikhha@epfl.ch>
2017-03-08 15:58:17 +0100

    Documentation for cold code motion transformation

--- ddbtoaster/pardis/transformer/ColdMotion.scala
+++ ddbtoaster/pardis/transformer/ColdMotion.scala
@@ -8,10 +8,15 @@ import ch.epfl.data.sc.pardis.effects._
 import ch.epfl.data.sc.pardis.utils.Graph
 import ddbt.lib.store.deep.StoreDSL
 
+/**
+ * Moves statements into cold regions whenever possible.
+ * For example, if there are some statements that can be moved inside a conditional
+ * branch, this transformer will move them inside that branch.
+ */
 class ColdMotion(override val IR: StoreDSL)  extends Optimizer[StoreDSL](IR) {
   import IR._
 
-  val debugger = true
+  val debugger = false
 
   def debug(msg: => Any): Unit = {
     if(debugger)
@@ -20,24 +25,21 @@ class ColdMotion(override val IR: StoreDSL)  extends Optimizer[StoreDSL](IR) {
 
   val effectAnalysis = new SideEffectsAnalysis(IR)
   val coldRegionAnalysis = new ColdRegionAnalysis(IR)
-  val countingAnalysis = new CountingAnalysis2(IR)
+  val countingAnalysis = new CountingAnalysisLocal(IR)
   type ColdRegion = coldRegionAnalysis.ColdRegion
 
   /**
    * First, it performs side effect analysis to have more precise effect information.
-   * Second, it identifes the hot regions from which statements should be hoisted 
-   * outside. Then, for each hot region specifies the list of statements that should
-   * be hoisted. Finally, it performs the transformation to rewrite the hot region
-   * to not include the hoisted statements, instead reifying them before the hot region.
+   * Second, it identifes the colde regions inside which some statements should be injected. 
+   * Then, for each cold region specifies the list of statements that should
+   * be injected. Finally, it performs the transformation to rewrite the cold regions
+   * to include the injected statements, instead of reifying them before that cold region.
    */
   def optimize[T: TypeRep](node: Block[T]): to.Block[T] = {
     effectAnalysis.optimize(node)
     coldRegionAnalysis.optimize(node)
-    // debug(coldRegionAnalysis.coldRegions)
     countingAnalysis.optimize(node)
     for((_, currentRegion) <- coldRegionAnalysis.coldRegions) {
-      // debug(currentRegion)
-      debug(currentRegion.symbol)
       currentHoistedStatements = Nil
       for(stm <- currentRegion.block.stmts.reverse) {
         for(v <- getFreeVars(stm.rhs)) {
@@ -45,34 +47,19 @@ class ColdMotion(override val IR: StoreDSL)  extends Optimizer[StoreDSL](IR) {
         }
       }
       if (currentHoistedStatements.nonEmpty) {
-        debug("injected statement symbols: " + currentHoistedStatements.reverse.map(_.sym))
         hoistedRegionStatements(currentRegion) = currentHoistedStatements.reverse
       }
-      
-      // debug(getFreeVars(cr.block.stmts.head.rhs).map(x => x -> countingAnalysis.symCounts(x)))
     }
-    // traverseBlock(node)
     transformProgram(node)
-    // node
   }
 
-  // def backwardAnalysis[T, C](node: Def[T]): Unit = {
-  //   node match {
-  //     case Block(stmts, res) =>
-  //       for(Stm(lhs, rhs) <- stmts.reverse) {
-  //         backwardAnalysis(rhs)
-  //       }
-  //     case _ =>
-
-  //   }
-  // }
+  /**
+   * Recursively goes backwards through the symbols so that it finds the dependent
+   * statements that can be moved inside the cold regions.
+   */
   def backwardAnalysis[T](sym: Rep[T]): Unit = {
     sym match {
       case Def(node) if countingAnalysis.symCounts(sym) == 1 && effectAnalysis.isPure(sym) => {
-        // debug("count is 1 for " + sym + "-->" + node)
-        // debug("is among cold regions " + coldRegionAnalysis.isColdSymbol(sym))
-        // debug("is pure " + effectAnalysis.isPure(sym))
-        // debug("freeVars: " + getFreeVars(node))
         for(s <- getFreeVars(node)) {
           backwardAnalysis(s)
         }
@@ -84,35 +71,15 @@ class ColdMotion(override val IR: StoreDSL)  extends Optimizer[StoreDSL](IR) {
   }
 
   /**
-   * Specifies the cold region surrounding the current context.
-   */
-  // var currentRegion: ColdRegion = _
-  /**
-   * The list of statements should be hoisted in the current context.
+   * The list of statements should be injected in the current cold region.
    */
   var currentHoistedStatements = List[Stm[_]]()
 
-  /**
-   * The list of variables bound in the current context.
-   */
-  // var currentBoundSymbols = collection.Set[Rep[_]]()
 
   /**
-   * Mapping between hot regions and the statements should be hoisted outside that region. 
+   * Mapping between cold regions and the statements should be inject inside that region. 
    */
   val hoistedRegionStatements = collection.mutable.Map[ColdRegion, List[Stm[_]]]()
-
-  /**
-   * Returns the symbols that the given definition is dependent on them.
-   */
-  // def getDependencies(node: Def[_]): List[Sym[Any]] =
-  //   node.funArgs.collect({
-  //     case s: Sym[Any] => List(s)
-  //     case b: Block[Any] => (b.res match {
-  //       case s: Sym[Any] => List(s)
-  //       case _ => Nil
-  //     }) ++ getStatements(b).flatMap(s => s.sym :: getDependencies(s.rhs)).distinct
-  //   }).flatten
 
   /**
    * Returns free variables occuring in a given definition.
@@ -140,129 +107,32 @@ class ColdMotion(override val IR: StoreDSL)  extends Optimizer[StoreDSL](IR) {
     }
 
   /**
-   * Specifies if the given statement can be hoisted from the current context.
-   */
-  // def isFloating(stm: Stm[_]): Boolean = {
-  //   effectAnalysis.isPure(stm.sym) && !stm.rhs.isInstanceOf[PardisLambdaDef] && 
-  //   getDependencies(stm.rhs).forall(x => !currentBoundSymbols.contains(x))
-  // }
-
-  /**
-   * Specifies if we are currently in a context in which we should perform hoisting.
-   */
-  // def isInMotionableContext(): Boolean = currentRegion != null
-
-  /**
-   * Extracts the statements defined in the given definition.
-   */
-  // def getStatements(n: Def[_]): List[Stm[_]] = n match {
-  //   case Block(stmts, res) => stmts ++ stmts.flatMap(x => getStatements(x.rhs))
-  //   case n if n.funArgs.exists(_.isInstanceOf[Block[_]]) => n.funArgs.collect({ case b: Block[_] =>
-  //     getStatements(b)
-  //   }).flatten
-  //   case _ => Nil
-  // }
-
-  /**
-   * Specifies if we have already hoisted a statement which contains the given statement.
-   * 
-   * For example, if the given statement is a statement inside an if-then-else expression
-   * which is already hoisted, there is no more need for hoisting the given statement.
-   * Hence, this method returns true in such cases.
-   * 
-   */
-  // def isContextAlreadyHoisted(stm: Stm[_]): Boolean = {
-  //   currentHoistedStatements.exists(cstm => getStatements(cstm.rhs).contains(stm))
-  // }
-
-  /**
-   * Collects the statements that should be hoisted from hot regions.
-   */
-  // override def traverseStm(stm: Stm[_]): Unit = stm match {
-  //   case Stm(sym, rhs) if coldRegionAnalysis.isHotSymbol(sym) => {
-  //     // Collecting the statements that should be hoisted for a hot region.
-  //     val previousRegion = currentRegion
-  //     currentRegion = coldRegionAnalysis.getColdRegion(sym)
-  //     val previousBoundSymbols = currentBoundSymbols
-  //     currentBoundSymbols = currentBoundSymbols ++ currentRegion.boundSymbols
-  //     val previousStatements = currentHoistedStatements
-  //     currentHoistedStatements = List()
-  //     traverseBlock(currentRegion.block)
-  //     if(currentHoistedStatements.nonEmpty)
-  //       hoistedRegionStatements(currentRegion) = currentHoistedStatements
-  //     currentRegion = previousRegion
-  //     currentBoundSymbols = previousBoundSymbols
-  //     currentHoistedStatements = previousStatements
-  //   }
-  //   case Stm(sym, rhs) if isInMotionableContext() => {
-  //     if(isFloating(stm)) {
-  //       // Gathers the statements that should be hoisted.
-  //       if(!isContextAlreadyHoisted(stm))
-  //         currentHoistedStatements +:= stm
-  //     } else {
-  //       // As it is not hoisted, everything depent on it also should not be hoisted.
-  //       // So we add its symbol to the list of bound symbols.
-  //       currentBoundSymbols = currentBoundSymbols + sym
-  //     }
-  //     super.traverseStm(stm)
-  //   }
-  //   case _ => super.traverseStm(stm)
-  // }
-
-  /**
-   * Updating the list of bound symbols.
-   */
-  // override def traverseBlock(block: Block[_]): Unit = {
-  //   val previousBoundSymbols = currentBoundSymbols
-  //   super.traverseBlock(block)
-  //   currentBoundSymbols = previousBoundSymbols
-  // }
-
-  /**
-   * Removes the hoisted statements from the hot region and reifies them outside
-   * the hot region.
+   * Removes the hoisted statements from their previous region, so that it can
+   * be reified inside the cold region as implemented in `transformBlockTyped`.
    */
   override def transformStmToMultiple(stm: Stm[_]): List[to.Stm[_]] =
     if (hoistedRegionStatements.exists(_._2.contains(stm.asInstanceOf[Stm[Any]])))
       Nil
     else 
-    // stm match {
-    //   // Removing the lambda expression of the hot region, as it should be reified
-    //   // after the expressions
-    //   // case Stm(sym, rhs) if hoistedRegionStatements.exists(_._1.lambda.exists(_._1 == sym)) => 
-    //   //   Nil
-    //   case Stm(sym, rhs) if hoistedRegionStatements.exists(_._1.symbol == sym) =>
-    //     val hotRegion = coldRegionAnalysis.coldRegions(sym)
-    //     val stmts = hoistedRegionStatements(hotRegion)
-    //     // Reifies the hoisted statements outside the hot region.
-    //     for(st <- stmts.reverse) {
-    //       reflectStm(st)
-    //     }
-    //     debug(s"${hotRegion.boundSymbols} -> ${stmts.size}")
-    //     // If the hot region contains a lambda, it transforms the lambda expression.
-    //     for((lambdaSym: Sym[Any], lambdaNode: Def[Any] with PardisLambdaDef) <- hotRegion.lambda) {
-    //       reflectStm(Stm(lambdaSym, transformDef(lambdaNode)(lambdaNode.tp))(lambdaNode.tp))
-    //     }
-    //     super.transformStmToMultiple(stm)
-    //   case _ =>
-        super.transformStmToMultiple(stm)
-    // }
+      super.transformStmToMultiple(stm)
 
-    override protected def transformBlockTyped[T: TypeRep, S: TypeRep](block: Block[T]): Block[S] = if(hoistedRegionStatements.exists(_._1.block == block)) {
-        
-        val coldRegion = hoistedRegionStatements.find(_._1.block == block).get._1
-        val newStmts = hoistedRegionStatements(coldRegion)
-        debug("happy for " + coldRegion.symbol)
-        reifyBlock[S] {
-          for(st <- newStmts) {
-            reflectStm(st)
-          }
-          block.stmts.foreach(transformStmToMultiple)
-          transformExp[T, S](block.res)
+  /**
+   * Reifies the statements that can be injected in the cold region. 
+   */
+  override protected def transformBlockTyped[T: TypeRep, S: TypeRep](block: Block[T]): Block[S] = 
+    if(hoistedRegionStatements.exists(_._1.block == block)) {
+      val coldRegion = hoistedRegionStatements.find(_._1.block == block).get._1
+      val newStmts = hoistedRegionStatements(coldRegion)
+      reifyBlock[S] {
+        for(st <- newStmts) {
+          reflectStm(st)
         }
-      } else {
-        super.transformBlockTyped(block)
+        block.stmts.foreach(transformStmToMultiple)
+        transformExp[T, S](block.res)
       }
+    } else {
+      super.transformBlockTyped(block)
+    }
 }
 
 class ColdRegionAnalysis(override val IR: StoreDSL) extends RuleBasedTransformer[StoreDSL](IR) {
@@ -278,6 +148,9 @@ class ColdRegionAnalysis(override val IR: StoreDSL) extends RuleBasedTransformer
 
   def getColdRegion(s: Rep[_]): ColdRegion = coldRegions(s)
 
+  /** 
+   * Specifies whether the given block should be considered as cold or not. 
+   */
   def isColdBlock[T](block: Block[T]): Boolean = 
     block.stmts match {
       case List(Stm(_, definition)) => definition match {
@@ -304,7 +177,7 @@ class ColdRegionAnalysis(override val IR: StoreDSL) extends RuleBasedTransformer
 /** A version of counting analysis which uses a local hash table for
   * for maintaing the number of usages of each symbol.
   */
-class CountingAnalysis2(override val IR: StoreDSL) extends RuleBasedTransformer[StoreDSL](IR) {
+class CountingAnalysisLocal(override val IR: StoreDSL) extends RuleBasedTransformer[StoreDSL](IR) {
   import IR._
 
   val symCounts = scala.collection.mutable.Map[Rep[_], Int]()
